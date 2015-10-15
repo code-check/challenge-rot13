0f70116d1bd103e5fd65fe645f17e7aeab3e1caf
NckHmml <NckHmml@users.noreply.github.com>
2015-10-15 11:49:52 +0900

    Added hidden test

---
+++ src/ROT13HiddenTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rot13Challenge.src
+{
+    [TestFixture]
+    public class ROT13HiddenTest : ROT13
+    {
+        [Test]
+        public void EncryptTest()
+        {
+            Assert.AreEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", Encrypt(Encrypt("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")));
+            Assert.AreEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", Encrypt("NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"));
+        }
+    }
+}
