ef2b2f0312191dd389043eda6699307bf2fb0447
NckHmml <NckHmml@users.noreply.github.com>
2015-10-15 11:17:28 +0900

    Added C# code

---
+++ src/ROT13.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rot13Challenge.src
+{
+    public class ROT13
+    {
+        //BEGIN_CHALLENGE
+        private char[] LookupTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+        private char[] TransformTable = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm".ToCharArray();
+        //END_CHALLENGE
+
+        public string Encrypt(string sIn)
+        {
+            // Write your code below here
+            //BEGIN_CHALLENGE
+            sIn = new string(sIn
+                .Select(x => {
+                    if (LookupTable.Contains(x))
+                        return TransformTable[Array.IndexOf(LookupTable, x)];
+                    return x;
+                })
+                .ToArray());
+            //END_CHALLENGE
+            return sIn;
+        }
+    }
+}
---
+++ src/ROT13Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rot13Challenge.src
+{
+    [TestFixture]
+    public class ROT13Test : ROT13
+    {
+        [Test]
+        public void EncryptTest()
+        {
+            Assert.AreEqual("Uryyb Jbeyq!", Encrypt("Hello World!"));
+        }
+    }
+}
